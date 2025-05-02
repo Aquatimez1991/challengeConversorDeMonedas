47c6196452c2eee2d2de5a08b2e81e85974fdebb
Aquatimez1991 <esriaden0.1@gmail.com>
2025-05-02 15:20:53 -0500

    Se agrega clase IntercambioDeMonedas para manejar el intercambio de las monedas predefinidas

---
+++ src/Servicios/IntercambioDeMonedas.java
@@ -0,0 +1,29 @@
+package Servicios;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class IntercambioDeMonedas {
+    private static final String API_KEY = "ef3b4b5516c9e616ff718200";
+    private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/";
+
+    public double convert(String from, String to, double amount) {
+        try {
+            String url = BASE_URL + from;
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+            JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
+            JsonObject conversionRates = json.getAsJsonObject("conversion_rates");
+            double rate = conversionRates.get(to).getAsDouble();
+            return rate * amount;
+        } catch (Exception e) {
+            System.out.println("Error en conversión: " + e.getMessage());
+            return 0.0;
+        }
+    }
+}
